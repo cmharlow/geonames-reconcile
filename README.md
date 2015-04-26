07bde68835918debbeca842078e3649c811289d8
Christina Harlow <cmh2166@columbia.edu>
2015-04-26 16:17:50 -0400

    first commit, minor API alters to fast-reconcile-master

---
+++ README.md
@@ -0,0 +1,8 @@
+An OpenRefine reconciliation service for [GeoNames](http://www.geonames.org/).
+
+The service queries the [GeoNames API](http://www.geonames.org/export/web-services.html)
+and provides normalized scores across queries for reconciling in Refine.
+
+Instructions for use coming soon.
+
+Michael Stephens wrote a [demo reconcilliation service](https://github.com/mikejs/reconcile-demo) and Ted Lawless wrote a [FAST reconciliation service](https://github.com/lawlesst/fast-reconcile) that this code basic repeats but for a different API. Please pay any thanks for this work to Ted Lawless first, and any complaints to Christina.
---
+++ reconcile.py
@@ -0,0 +1,197 @@
+"""
+An OpenRefine reconciliation service for the API provided GeoNames.
+
+GeoNames API documentation: http://www.geonames.org/export/web-services.html
+
+This code is adapted from the wonderful Ted Lawless: https://github.com/lawlesst/fast-reconcile
+"""
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+import json
+from operator import itemgetter
+import urllib
+
+from fuzzywuzzy import fuzz
+import requests
+
+app = Flask(__name__)
+
+api_base_url = 'http://api.geonames.org/searchJSON?username=charlow2&isNameRequired=yes&'
+geonames_uri_base = 'http://sws.geonames.org/{0}'
+
+#If it's installed, use the requests_cache library to
+#cache calls to the GeoNames API.
+try:
+    import requests_cache
+    requests_cache.install_cache('geonames_cache')
+except ImportError:
+    app.logger.debug("No request cache found.")
+    pass
+
+#Helper text processing
+import text
+
+#Map the GeoNames query indexes to service types
+default_query = {
+    "id": "/geonames/all",
+    "name": "All GeoNames terms",
+    "index": "q"
+}
+
+refine_to_geonames = [
+    {
+        "id": "/geonames/name",
+        "name": "Place Name",
+        "index": "name"
+    },
+    {
+        "id": "/geonames/name_startsWith",
+        "name": "Place Name Starts With",
+        "index": "name_startsWith"
+    },
+    {
+        "id": "/geonames/name_equals",
+        "name": "Exact Place Name",
+        "index": "name_equals"
+    },
+    {
+        "id": "/geonames/country",
+        "name": "Country Code (ISO-3166) Parameter",
+        "index": "country"
+    },
+    {
+        "id": "/geonames/countryBias",
+        "name": "Country Bias Parameter",
+        "index": "countryBias"
+    },
+    {
+        "id": "/geonames/countryBias",
+        "name": "Search Language Parameter",
+        "index": "searchlang"
+    }
+]
+refine_to_geonames.append(default_query)
+
+
+#Make a copy of the GeoNames mappings.
+query_types = [{'id': item['id'], 'name': item['name']} for item in refine_to_geonames]
+
+# Basic service metadata. There are a number of other documented options
+# but this is all we need for a simple service.
+metadata = {
+    "name": "GeoNames Reconciliation Service",
+    "defaultTypes": query_types,
+    "view": {
+        "url": "{{id}}"
+    }
+}
+
+
+def make_uri(geonames_id):
+    """
+    Prepare a GeoNames url from the ID returned by the API.
+    """
+    geonames_uri = geonames_uri_base.format(geonames_id)
+    return geonames_uri
+
+
+def jsonpify(obj):
+    """
+    Helper to support JSONP
+    """
+    try:
+        callback = request.args['callback']
+        response = app.make_response("%s(%s)" % (callback, json.dumps(obj)))
+        response.mimetype = "text/javascript"
+        return response
+    except KeyError:
+        return jsonify(obj)
+
+
+def search(raw_query, query_type='/geonames/all'):
+    """
+    Hit the GeoNames API for names.
+    """
+    out = []
+    unique_geonames_ids = []
+    query = text.normalize(raw_query)
+    query_type_meta = [i for i in refine_to_geonames if i['id'] == query_type]
+    if query_type_meta == []:
+        query_type_meta = default_query
+    query_index = query_type_meta[0]['index']
+    try:
+        url = api_base_url + query_index  + '=' + urllib.quote(query)
+        app.logger.debug("GeoNames API url is " + url)
+        resp = requests.get(url)
+        results = resp.json()
+    except Exception, e:
+        app.logger.warning(e)
+        return out
+    for position, item in enumerate(results['geonames']):
+        match = False
+        name = item.get('name')
+        alternate = item.get('toponymName')
+        if (len(alternate) > 0):
+            alt = alternate[0]
+        else:
+            alt = ''
+        geonames_id = item.get('geonameId')
+        geonames_uri = make_uri(geonames_id)
+        #The GeoNames service returns some duplicates.  Avoid returning many of the
+        #same result
+        if geonames_id in unique_geonames_ids:
+            continue
+        else:
+            unique_geonames_ids.append(geonames_id)
+        score_1 = fuzz.token_sort_ratio(query, name)
+        score_2 = fuzz.token_sort_ratio(query, alt)
+        #Return a maximum score
+        score = max(score_1, score_2)
+        if query == text.normalize(name):
+            match = True
+        elif query == text.normalize(alt):
+            match = True
+        resource = {
+            "id": geonames_uri,
+            "name": name,
+            "score": score,
+            "match": match,
+            "type": query_type_meta
+        }
+        out.append(resource)
+    #Sort this list by score
+    sorted_out = sorted(out, key=itemgetter('score'), reverse=True)
+    #Refine only will handle top three matches.
+    return sorted_out[:3]
+
+
+@app.route("/reconcile", methods=['POST', 'GET'])
+def reconcile():
+    # If a 'queries' parameter is supplied then it is a dictionary
+    # of (key, query) pairs representing a batch of queries. We
+    # should return a dictionary of (key, results) pairs.
+    queries = request.form.get('queries')
+    if queries:
+        queries = json.loads(queries)
+        results = {}
+        for (key, query) in queries.items():
+            qtype = query.get('type')
+            if qtype is None:
+                return jsonpify(metadata)
+            data = search(query['query'], query_type=qtype)
+            results[key] = {"result": data}
+        return jsonpify(results)
+    # If neither a 'query' nor 'queries' parameter is supplied then
+    # we should return the service metadata.
+    return jsonpify(metadata)
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    oparser = OptionParser()
+    oparser.add_option('-d', '--debug', action='store_true', default=False)
+    opts, args = oparser.parse_args()
+    app.debug = opts.debug
+    app.run(host='0.0.0.0')
